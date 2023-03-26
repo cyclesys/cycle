f4d86b381ca233fc0c1c767149b1ca3b62843d95
Diego Covarrubias <dcov@pm.me>
2023-03-26 11:01:17 -0700

    [channel] update ChannelView serialization logic to use ChannelSerializer and ChannelDeserializer, and write test for it.

--- lib/src/channel/view.rs
+++ lib/src/channel/view.rs
@@ -1,12 +1,14 @@
-use std::slice;
+use serde::{Deserialize, Serialize};
+use std::{mem, slice};
 
 use windows::Win32::{
     Foundation::HANDLE,
     System::Memory::{MapViewOfFile, FILE_MAP_ALL_ACCESS},
 };
 
-use super::{Error, Result, WindowsError};
+use super::{de::ChannelDeserializer, ser::ChannelSerializer, Error, Result, WindowsError};
 
+#[derive(Serialize, Deserialize)]
 pub struct ChannelMessage<'a> {
     pub bytes_left: usize,
     pub bytes: &'a [u8],
@@ -25,8 +27,8 @@ impl ChannelView {
 
 impl ChannelView {
     pub const SIZE: usize = 4 * 1024;
-    // channel_size - message_state - write_size - bytes_left = 4085
-    pub const MAX_WRITE: usize = Self::SIZE - 1 - 2 - 8;
+    // channel_size - bytes_left - bytes_len
+    pub const MAX_WRITE: usize = Self::SIZE - (mem::size_of::<usize>() * 2);
 
     pub fn create(file: HANDLE) -> Result<Self> {
         let ptr = unsafe { MapViewOfFile(file, FILE_MAP_ALL_ACCESS, 0, 0, Self::SIZE) };
@@ -41,52 +43,41 @@ impl ChannelView {
 
     pub fn read(&self) -> Result<ChannelMessage> {
         let bytes = unsafe { slice::from_raw_parts(self.ptr, Self::SIZE) };
-        let mut cursor: usize = 0;
-        let mut read_bytes = |size: usize| -> &[u8] {
-            let bytes = &bytes[cursor..(cursor + size)];
-            cursor += size;
-            bytes
-        };
-
-        let write_size = {
-            let mut payload_size = [0; 2];
-            payload_size.copy_from_slice(read_bytes(2));
-            u16::from_le_bytes(payload_size) as usize
-        };
-
-        if write_size > Self::MAX_WRITE {
-            return Err(Error::ChannelInvalidState);
-        }
-
-        let bytes_left = {
-            let mut bytes_left = [0; 8];
-            bytes_left.copy_from_slice(read_bytes(8));
-            usize::from_le_bytes(bytes_left)
-        };
-
-        Ok(ChannelMessage {
-            bytes_left,
-            bytes: read_bytes(write_size),
-        })
+        ChannelDeserializer::deserialize(bytes)
     }
 
-    pub fn write(&mut self, message: ChannelMessage) -> Result<()> {
-        let bytes = unsafe { slice::from_raw_parts_mut(self.ptr, Self::SIZE) };
-        let mut cursor: usize = 0;
-        let mut write_bytes = |new_bytes: &[u8]| {
-            bytes[cursor..(cursor + new_bytes.len())].copy_from_slice(new_bytes);
-            cursor += new_bytes.len();
+    pub fn write(&self, message: ChannelMessage) -> Result<()> {
+        // TODO: serialize directly into chan_bytes
+        let msg_bytes = ChannelSerializer::serialize(&message)?;
+        let chan_bytes = unsafe { slice::from_raw_parts_mut(self.ptr, Self::SIZE) };
+        let write_bytes = &mut chan_bytes[0..msg_bytes.len()];
+        write_bytes.copy_from_slice(&msg_bytes);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{ChannelMessage, ChannelView, HANDLE};
+
+    #[test]
+    fn channel_view_read_write() {
+        let mut bytes = [0; ChannelView::SIZE];
+        let view = ChannelView {
+            // not used
+            file: HANDLE(0),
+            ptr: bytes.as_mut_ptr(),
         };
 
-        if message.bytes.len() > Self::MAX_WRITE {
-            return Err(Error::ChannelInvalidWrite);
-        }
+        view.write(ChannelMessage {
+            bytes_left: 100,
+            bytes: &[10, 20, 30, 40, 50],
+        })
+        .unwrap();
 
-        let write_size = message.bytes.len() as u16;
-        write_bytes(&write_size.to_le_bytes());
-        write_bytes(&message.bytes_left.to_le_bytes());
-        write_bytes(message.bytes);
+        let message = view.read().unwrap();
 
-        Ok(())
+        assert_eq!(message.bytes_left, 100);
+        assert_eq!(message.bytes, &[10, 20, 30, 40, 50]);
     }
 }
