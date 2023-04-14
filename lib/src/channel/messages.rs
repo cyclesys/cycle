1f0029bad03f4ff1003a11d490be9a3d0424666a
Diego Covarrubias <dcov@pm.me>
2023-04-14 09:30:53 -0700

    remove rust files

--- lib/src/channel/messages.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-pub enum SystemMessage {}
-
-#[derive(Serialize, Deserialize)]
-pub enum PluginMessage {}
--- lib/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod channel;
-pub mod define;
-pub mod render;
-
-pub use libcycle_define::define;
